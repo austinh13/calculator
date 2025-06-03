b810635f54e7cb3a5e18317ac90d80ca3016ed1c
austinh13 <austinha236@gmail.com>
2025-06-03 16:49:25 -0500

    added simple display

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const solution = new Array();
+var equation = new Array();
+var counter = 0;
+equation.push(0);
+
 function buttonClick(content){
-    solution.push(content);
-    for(let i = 0; i < solution.length;i++)
-    {
-        console.log(solution[i]);
+    var display = document.getElementById("lowerDisplay")
+    if((content >= 0) && (content <= 9)){
+        equation[counter] = equation[counter] * 10 + Number(content);
+        display.innerHTML = equation[counter];
+    }
+    else{
+
     }
 }
